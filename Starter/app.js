3babf952a1161f3e081048e4f5a5e9d041c56747
tarwaxur <karahanbugrahan909@gmail.com>
2024-06-24 21:07:51 +0300

    Starter

--- Starter/app.js
+++ Starter/app.js
@@ -14,3 +14,12 @@ alert("Please press the 'OK' button to continue.");
 alert("Please press the 'OK' button to continue.");
 
 console.log("Hi, my name is Buğrahan, surname is Karahan");
+
+// ! Turkish: "alert" fonksiyonu ile etkileşime girdikten sonra "console.error" fonksiyonunu çalıştırma.
+// ! English: Running the "console.error" function after interacting with the "alert" function.
+
+alert("Click the 'OK' button for the system to crash.");
+
+console.error(" Since you pressed the 'OK' button, the system is closed.");
+
+alert("This project has come to this, as its name suggests, it is just an experiment. See you later!");
