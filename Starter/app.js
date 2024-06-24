ce890723b0d21fa1261c654ad177b8bfd784be8c
tarwaxur <karahanbugrahan909@gmail.com>
2024-06-24 20:41:35 +0300

    Starter

---
+++ Starter/app.js
@@ -0,0 +1,9 @@
+// ! Turkish: "console.log" fonksiyonunu kullanma.
+// ! English: Using the "console.log" function.
+
+console.log("Hello world!");
+
+// ! Turkish: "alert" fonksiyonunu kullanma.
+// ! English: Using the "alert" function.
+
+alert("Please press the 'OK' button to continue.");
