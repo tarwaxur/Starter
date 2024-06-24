a691e4e9335a93c7bda19ff6f7bbe7db2580a18c
tarwaxur <karahanbugrahan909@gmail.com>
2024-06-24 20:45:35 +0300

    Starter

--- Starter/app.js
+++ Starter/app.js
@@ -7,3 +7,10 @@ console.log("Hello world!");
 // ! English: Using the "alert" function.
 
 alert("Please press the 'OK' button to continue.");
+
+// ! Turkish: "alert" fonksiyonu ile etkileşime girdikten sonra "console.log" fonksiyonunu çalıştırma.
+// ! English: Running the "console.log" function after interacting with the "alert" function.
+
+alert("Please press the 'OK' button to continue.");
+
+console.log("Hi, I'm tarwaxur and I'm 16 years old.");
